e0c2af24423c55313acb9c084451573b7785eeae
santosh <santosh.mutyala.dev@gmail.com>
2024-11-15 21:37:07 +0100

    step 3 added admin router

--- week-8/index.js
+++ week-8/index.js
@@ -2,14 +2,16 @@ const express = require("express");
 // create an instance of express http server
 const app = express();
 
-const userRouter = require("./routes/user");
-const courseRouter = require("./routes/courses");
+const { userRouter } = require("./routes/user");
+const { courseRouter } = require("./routes/courses");
+const { adminRouter } = require("./routes/admin");
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/user", userRouter);
-app.use("/course", courseRouter);
+app.use("/course", courseRouter);   
+app.use("/admin", adminRouter);   
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
---
+++ week-8/routes/admin.js
@@ -0,0 +1,27 @@
+const {Router} = require("express");
+
+const adminRouter = Router();
+
+adminRouter.post("/signup", async (req, res) => {
+
+}) 
+
+adminRouter.post("/login", async (req, res) => {
+    
+}) 
+
+adminRouter.post("/course", async (req, res) => {
+    
+}) 
+
+adminRouter.put("/course", async (req, res) => {
+    
+}) 
+
+adminRouter.get("/course/bulk", async (req, res) => {
+    
+}) 
+
+module.exports = {
+    adminRouter
+}
