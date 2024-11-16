b23eaad461a84daaea3458356074df4a84b88a76
santosh <santosh.mutyala.dev@gmail.com>
2024-11-16 11:41:44 +0100

    added user signup and signin validation

--- week-8/routes/user.js
+++ week-8/routes/user.js
@@ -1,12 +1,14 @@
 const {Router} = require("express");
-const {userSchemaValidation} = require("../schemaValidation/userSchemaValidation");
+const bcrypt = require("bcrypt");
+const {userSignupSchemaValidation, userSigninSchemaValidation} = require("../schemaValidation/userSchemaValidation");
+const { User } = require("../db/db");
 
 const userRouter = Router();
 
 userRouter.post("/signup", async (req, res) => {
     const {firstName, lastName, email, password} = req.body;
 
-    const {success, error} = userSchemaValidation.safeParse(req.body);
+    const {success, error} = userSignupSchemaValidation.safeParse(req.body);
     
     // early return if validation fails
     if(!success){
@@ -17,14 +19,33 @@ userRouter.post("/signup", async (req, res) => {
         })
     }
 
-    // 
+    // early return if the user already exists
+    const existingUser = await User.findOne({email: email})
+    console.log(existingUser)
+    if(existingUser) {
+        return res.json({
+            msg: `User with ${email} already exists`
+        })
+    }
 
-    
 
+    // else create a user in the database
+    const hashedpassword = await bcrypt.hash(password, 5);
+    const user = await User.create({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: hashedpassword
+    })
+
+    res.status(200).json({
+        msg: `${firstName}, you're signed up`
+    })
 
 }) 
 
-userRouter.post("/login", async (req, res) => {
+userRouter.post("/signin", async (req, res) => {
+    
     
 }) 
 
--- week-8/schemaValidation/userSchemaValidation.js
+++ week-8/schemaValidation/userSchemaValidation.js
@@ -1,6 +1,6 @@
 const zod = require("zod");
 
-const userSchemaValidation = zod.object({
+const userSignupSchemaValidation = zod.object({
     firstName: zod
                 .string()
                 .min(3, {message: "First name must contain at least 3 characters"}),
@@ -22,6 +22,24 @@ const userSchemaValidation = zod.object({
                 .refine(password => /[^\w\s]/.test(password), {message: "Password must contain at least one special character"})
 }).strict({message: "Unknown parameters have been passed"})
 
+
+const userSigninSchemaValidation = zod.object({
+    email: zod
+            .string()
+            .email({ message: "Invalid email address" })
+            .min(8, {message: "Email must be at least 8 characters long"})
+            .max(100, {message: "Email length cannot exceed 100 characters"}),
+    password: zod
+                .string()
+                .min(8, {message: "Password must contain at least 8 characters"})
+                .max(50, {message: "Password length cannot exceed 50 characters"})
+                .refine(password => /[A-Z]/.test(password), {message: "Password must contain at least one uppercase letter"})
+                .refine(password => /[a-z]/.test(password), {message: "Password must contain at least one lowercase letter"})
+                .refine(password => /[0-9]/.test(password), {message: "Password must contain at least one number"})
+                .refine(password => /[^\w\s]/.test(password), {message: "Password must contain at least one special character"})
+}).strict({message: "Unknown parameters have been passed"})
+
 module.exports = {
-    userSchemaValidation
+    userSignupSchemaValidation,
+    userSigninSchemaValidation
 }
