5729345a9ec080e725e6fa1187b39aa7910a176b
santosh <santosh.mutyala.dev@gmail.com>
2024-11-20 12:27:33 +0100

    Linkedin post component

---
+++ week-9/linkedinPost/src/App.jsx
@@ -0,0 +1,35 @@
+import  Post  from "./Post";
+import { useState } from "react";
+
+
+function App() {
+
+  const [posts, setPosts] = useState([]);
+
+  function addPost(){
+    console.log("in fun")
+    setPosts([...posts, {
+      img: "https://media.licdn.com/dms/image/v2/D4D03AQFyMu9_B-Ts2A/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1715176146284?e=1737590400&v=beta&t=xhVIe0dNNR5k-n9DpggYTm-POTddv7huWt9TUe_ebLk",
+      name: "Santosh Mutyala",
+      bio: "Fullstack developer and CEO at Tech@Berlin",
+      body: "The only constant in life is change. Whether it's learning a new skill, adapting to a new role, or navigating unexpected challenges, growth comes from stepping out of your comfort zone. What change have you embraced recently, and how has it transformed you? Let's inspire each other!",
+      time: "40m",
+      bodyImg: "https://media.licdn.com/dms/image/v2/D4E22AQHFtndZAkr-sg/feedshare-shrink_800/feedshare-shrink_800/0/1732044011952?e=1735171200&v=beta&t=opHBW5HJT5aMzwf3-oOgo18YC6Wv-xEGdvGskeTQc0A"
+    }])
+    console.log("after setposts")
+  }
+
+
+  return (
+    <div style={{backgroundColor: "#F3F2EF", height: "100vh"}}>
+      <button onClick={addPost}>Add post</button>
+      {posts.map(post => {
+       return(<Post name={post.name} bio={post.bio} body={post.body} time={post.time} bodyImg={post.bodyImg} img={post.img} />);
+      })}
+    </div>
+  )
+}
+
+
+
+export default App
---
+++ week-9/linkedinPost/src/Post.jsx
@@ -0,0 +1,34 @@
+function Post(props){
+    return (
+      // post container
+      <div style={{height: 400, width: 555, borderRadius: 10, backgroundColor: "white", margin: "0 auto"}}>
+  
+        {/* top level header for say - image and bio */}
+        <div style={{display: "flex", padding: 10}}>
+          <img src={props.img} style={{height: 50, width: 50, borderRadius: 50, marginRight: 10}} alt="post-profile" />
+  
+          <div>
+            <div style={{fontSize: 16, fontWeight: 600}}>
+              {props.name}
+            </div>
+            <div style={{fontSize: 14}}>
+              {props.bio}
+            </div>
+            <div style={{fontSize: 12}}>
+            {props.time}
+            </div>
+          </div>
+        </div>
+        
+        {/* body */}
+        <div>
+          <div style={{padding: 10, lineHeight: 1.2}}>
+            {props.body}
+          </div>
+          {props.bodyImg && <img src={props.bodyImg} style={{height: 200, width: 554 }} alt="BodyImg" />}
+        </div>
+      </div>
+    )
+}
+
+export default Post;
