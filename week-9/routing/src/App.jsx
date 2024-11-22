306c4d1e820fcd7bc076b0ae2bf4c1ad77022f0a
santosh <santosh.mutyala.dev@gmail.com>
2024-11-22 10:15:59 +0100

    add layout using Outlet

--- week-9/routing/src/App.jsx
+++ week-9/routing/src/App.jsx
@@ -1,26 +1,51 @@
 import { useEffect } from 'react';
-import { BrowserRouter, Routes, Route, Link, useNavigate, redirect } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 
 
 function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <Link to="/">Allen</Link>
-        |
-        <Link to="/neet/online-coaching-class-11">Class 11</Link>
-        |
-        <Link to="/neet/online-coaching-class-12">Class 12</Link>
+      <BrowserRouter> {/* BrowserRouter has Routes has children, Routes has Route. week 9.2 */}
         <Routes>
-          <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
-          <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
-          <Route path="/" element={<Landing />} />
-          <Route path="*" element={<ErrorPage />} />
+          <Route path="/" element={<Layout />}>
+            <Route path="/neet/online-coaching-class-11" element={<Class11Program />} />
+            <Route path="/neet/online-coaching-class-12" element={<Class12Program />} />
+            <Route path="/" element={<Landing />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
   )
+}
+
+function Layout(){
+  return (
+    <div style={{height: "100vh"}}>
+      <Header/>
+      <div style={{height: "80vh", backgroundColor: "red"}}>
+        <Outlet/> {/* Outlet renders the elements */}
+      </div>
+        <Footer/>
+    </div>
+  )
+}
+
+function Header(){
+  return <>
+    <Link to="/">Allen</Link>
+    |
+    <Link to="/neet/online-coaching-class-11">Class 11</Link>
+    |
+    <Link to="/neet/online-coaching-class-12">Class 12</Link>
+  </>
+}
+
+function Footer(){
+  return <>
+    <h1>This is the footer for now</h1>
+  </>
 }
 
 function Class11Program(){
