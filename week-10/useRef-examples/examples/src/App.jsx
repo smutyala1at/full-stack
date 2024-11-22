733235f8a14816e7f3a80c90240bf6d89ddc6133
santosh <santosh.mutyala.dev@gmail.com>
2024-11-22 14:43:30 +0100

    implement timer using useState hook - inefficient

--- week-10/useRef-examples/examples/src/App.jsx
+++ week-10/useRef-examples/examples/src/App.jsx
@@ -1,13 +1,20 @@
 
 import { useState, useRef } from 'react'
 
+
 function App() {
   const [count, setCount] = useState(0)
-  const clockRef = useRef(null); /* useRef return object with "current" property, and why we use this is, useRef persists over re-renders and you could use useState, but downside of it is, whenever the values changes, it does unnecessary re-render */
+  const clockRef = useRef(null); 
+  /* useRef return object with "current" property, and why we use this is, useRef persists over re-renders and you could use useState, but downside of it is, whenever the values changes, it does unnecessary re-render  */
 
   function startClock(){
     /* usually react doesn't go crazy although we are using setInterval here, because it is only triggered when we click on start button,
-    when re-rendering, it can't invoke a function. But it does go crazy if we click on start button several times */
+    when re-rendering, it can't invoke a function. But it does go crazy if we click on start button several times  */
+
+    /* use useRef WHEN YOU ARE MANAGING NOT UI ELEMENTS. HERE REFERENCE TO CLEARINTERVAL IS NOT BEING USED IN THE UI*/
+
+    if(clockRef.current) return  /* return if the clock is already running, to prevent unnecessary clocks */
+
     clockRef.current = setInterval(function(){
       setCount(c => c + 1);
     }, 1000);
@@ -15,6 +22,7 @@ function App() {
 
   function stopClock(){
     clearInterval(clockRef.current);
+    clockRef.current = null; // update reference to null, so that user can start again 
   }
 
   return (
@@ -26,5 +34,35 @@ function App() {
   )
 }
 
+/* 
+  function App(){
+    const [time, setTime] = useState(0);
+    const [ref, setRef] = useState(null);
+  
+    function startTimer(){
+  
+      if(ref) return;
+  
+      let timerRef;
+      timerRef = setInterval(function(){
+        setTime(time => time + 1);
+      }, 1000);
+      setRef(timerRef);
+    }
+  
+    function stopTimer(){
+      clearInterval(ref);
+      setRef(null);
+    }
+  
+    return(
+      <>
+        <h1>{time}</h1>
+        <button onClick={startTimer}>start</button>
+        <button onClick={stopTimer}>stop</button>
+      </>
+    )
+  }
+ */
 
 export default App
