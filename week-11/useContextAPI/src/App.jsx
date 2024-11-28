1e1fc9119b77a0dccbc693202563f93ed58413ee
santosh <santosh.mutyala.dev@gmail.com>
2024-11-28 19:36:03 +0100

    simple useContext API implementation

---
+++ week-11/useContextAPI/src/App.jsx
@@ -0,0 +1,57 @@
+import { createContext, useContext, useState } from 'react'
+
+const valueContext = createContext();
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <valueContext.Provider value={{count, setCount}}>
+        <Parent />
+      </valueContext.Provider>
+    </div>
+  )
+}
+
+function Parent(){
+  return (
+    <div>
+      <Increase />
+      <Decrease />
+      <Value />
+    </div>
+  )
+}
+
+function Increase(){
+  const {setCount} = useContext(valueContext);
+
+  return (
+    <div>
+      <button onClick={() => setCount(c => c + 1)}>Increase</button>
+    </div>
+  )
+}
+
+function Decrease(){
+  const {setCount} = useContext(valueContext);
+
+  return (
+    <div>
+      <button onClick={() => setCount(c => c - 1)}>Decrease</button>
+    </div>
+  )
+}
+
+function Value(){
+  const {count} = useContext(valueContext);
+
+  return (
+    <div>
+      {count}
+    </div>
+  )
+}
+
+export default App
