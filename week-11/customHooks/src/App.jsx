95438e26e294599a62c1c62fccb54377d17b959c
santosh <santosh.mutyala.dev@gmail.com>
2024-11-28 13:45:04 +0100

    better implementation of usePrev

--- week-11/customHooks/src/App.jsx
+++ week-11/customHooks/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { useFetch }from './hooks/UseFetchHook'
 import { usePrev } from './hooks/UsePrevHook';
+import { useDebounce } from './hooks/UseDebounceHook';
 
 // custom hook
 function useCounter(){
@@ -28,6 +29,7 @@ function Counter() {
 
 function FetchPost() {
   const [currentPost, setCurrentPost] = useState(1);
+  const [anything, setAnything]= useState(1)
   const {post, loading} = useFetch('https://jsonplaceholder.typicode.com/todos/' + currentPost, 10);
   const prevPost = usePrev(post);
 
@@ -40,6 +42,7 @@ function FetchPost() {
       <button onClick={() => setCurrentPost(1)}>Post 1</button>
       <button onClick={() => setCurrentPost(2)}>Post 2</button>
       <button onClick={() => setCurrentPost(3)}>Post 3</button>
+      <button onClick={() => setAnything(c => c + 1)}>rerender</button>
       <div>
         {JSON.stringify(post)}
       </div>
@@ -50,10 +53,29 @@ function FetchPost() {
   )
 }
 
+function Search(){
+  const [inputVal, setInputVal] = useState("");
+  const debouncedResponse = useDebounce(inputVal, 2); 
+
+  function handleInputChange(e){
+    setInputVal(e.target.value);
+  }
+
+  return (
+  <div>
+    <input onInput={handleInputChange} type="text" placeholder='search'/>
+    <div>
+      {debouncedResponse}
+    </div>
+  </div>
+  )
+}
+
 function App(){
   return <>
     <Counter />
     <FetchPost />
+    <Search />
   </>
 }
 
--- week-11/customHooks/src/hooks/UsePrevHook.jsx
+++ week-11/customHooks/src/hooks/UsePrevHook.jsx
@@ -1,13 +1,15 @@
-import {useRef, useEffect} from 'react';
+import {useRef} from 'react';
 
 export function usePrev(value){
-    const ref = useRef({}); // initialises on first call, and then this is ignored on subsequent calls
+    const ref = useRef({ latest: value, previous: "" }); // initialises on first call, and then this is ignored on subsequent calls
 
-    useEffect(function(){
-        ref.current = value;
-    }, [value]);
+    // using json stringify because when the backend is fetching every 10 seconds, the object reference is changing. using just ===, if condition is passed because JS checks for object references rather than checking if the contents are same
+    if(JSON.stringify(ref.current.latest) !== JSON.stringify(value)){ // on first run this condition will be failed, as we initializing latest with props value. Usually on later calls, latest is different than props value so this if condition will be passed, if uneccessary re-renders happen component which holds usePrev hook, then it calls usePrev with prop value which is indeed equal to latest, then this condition will be failed
+        ref.current.previous = ref.current.latest
+        ref.current.latest = value
+    } 
 
-    return ref.current;
+    return ref.current.previous; 
 } // in react, it returns first, effect gets called later
 // when first post is passed as arg on first call, it returns {} and then runs effect, where it sets val to first post
 
