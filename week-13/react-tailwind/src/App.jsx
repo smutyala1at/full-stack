f1119ba7a816e47d1af9fb617be515dbfba7814d
santosh <santosh.mutyala.dev@gmail.com>
2024-12-01 16:31:26 +0100

    implement flex using tailwind

---
+++ week-13/react-tailwind/src/App.jsx
@@ -0,0 +1,23 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className='flex justify-between'>
+        <div className="bg-blue-500">
+          Hello from first div
+        </div>
+        <div className="bg-red-500">
+          Hello from second div
+        </div>
+        <div className="bg-green-500">
+          Hello from third div
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
