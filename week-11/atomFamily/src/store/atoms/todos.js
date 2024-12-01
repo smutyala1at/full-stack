d11f82ea0794ee599cb99c7782a2ef3c06451293
santosh <santosh.mutyala.dev@gmail.com>
2024-12-01 14:48:49 +0100

    use selectorFamily

--- week-11/atomFamily/src/store/atoms/todos.js
+++ week-11/atomFamily/src/store/atoms/todos.js
@@ -1,14 +1,14 @@
-import { atomFamily, selector } from "recoil";
+import { atomFamily, selectorFamily } from "recoil";
 import axios from "axios";
 
 // atomFamily -  Creates separate storage (atoms) for each todo, identified by its id
 export const todosAtom = atomFamily({
-    key: "todosAtomFamily",
-    default: (id) => selector({
+    key: "todoAtomFamily",
+    default: selectorFamily({
         // selector: Defines how to fetch or derive the data for each todo
-        key:`todosAtomSelector_${id}`,
+        key: "todoSelectorFamily",
         // Ensures each todo’s data fetching is unique and separate, so Recoil can cache and manage each todo's data individually.
-        get: async () => {
+        get: (id) => async () => {
             const response = await axios.get(`http://localhost:3000/todos/${id}`);
             return response.data;
         }
