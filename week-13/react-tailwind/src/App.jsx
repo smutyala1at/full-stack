c7195238c72239e5d2fab620d709c1124c60d476
santosh <santosh.mutyala.dev@gmail.com>
2024-12-01 17:00:50 +0100

    add responsiveness

--- week-13/react-tailwind/src/App.jsx
+++ week-13/react-tailwind/src/App.jsx
@@ -5,14 +5,15 @@ function App() {
 
   return (
     <>
-      <div className='grid grid-cols-12'>
-        <div className="bg-blue-500 col-span-5">
+      {/* tailwind is mobile first, grid grid-cols-1, means, only single child span over whole grid, from sm breakpoint and above grid has 12 cols */}
+      <div className='grid grid-cols-1 sm:grid-cols-12'>
+        <div className="bg-blue-500 col-span-1 sm:col-span-5">
           Hello from first div
         </div>
-        <div className="bg-red-500 col-span-2">
+        <div className="bg-red-500 col-span-1 sm:col-span-2">
           Hello from second div
         </div>
-        <div className="bg-green-500 col-span-5">
+        <div className="bg-green-500 col-span-1 sm:col-span-5">
           Hello from third div
         </div>
       </div>
