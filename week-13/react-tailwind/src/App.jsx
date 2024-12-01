fd4367501a6991d9fb89c5bf2aef2d851eeb66f8
santosh <santosh.mutyala.dev@gmail.com>
2024-12-01 16:42:17 +0100

    implement grid using tailwind

--- week-13/react-tailwind/src/App.jsx
+++ week-13/react-tailwind/src/App.jsx
@@ -5,14 +5,14 @@ function App() {
 
   return (
     <>
-      <div className='flex justify-between'>
-        <div className="bg-blue-500">
+      <div className='grid grid-cols-12'>
+        <div className="bg-blue-500 col-span-5">
           Hello from first div
         </div>
-        <div className="bg-red-500">
+        <div className="bg-red-500 col-span-2">
           Hello from second div
         </div>
-        <div className="bg-green-500">
+        <div className="bg-green-500 col-span-5">
           Hello from third div
         </div>
       </div>
