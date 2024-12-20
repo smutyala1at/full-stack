b45719e46aa1f34e31fd3b9654fba6b3db04c87c
Santosh Mutyala <santosh.mutyala@uni-oldenburg.de>
2024-12-20 08:38:20 +0100

    add user signin functionality

--- todos-project/backend/routes/userRouter.js
+++ todos-project/backend/routes/userRouter.js
@@ -1,30 +1,86 @@
+require("dotenv").config();
 const { Router } = require("express");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const { User, Todos } = require("../db/db");
 
-
+// create new router instance
 const userRouter = Router();
 
+// signup endpoint - create new user
 userRouter.post("/signup", async (req, res) => {
-    const { firstName, lastName, email, password } = req.body;
+    try {
+        const { firstName, lastName, email, password } = req.body;
 
-    const user = User.findOne({ email: email });
+        // find if user exists
+        const user = User.findOne({ email: email });
+        if (user) {
+            return res.status(400).json({
+                message: `user with email ${email} already exists`
+            })
+        }
 
-    if (user) {
-        return res.status(400).json({
-            message: `user with email ${email} already exists`
+        // hash password and create user
+        const hashedPassword = await bcrypt.hash(password, 5);
+        await User.create({
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: hashedPassword
+        })
+
+        res.status(201).json({
+            messaage: `${firstName}, you're succesfully signed up`
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Something went wrong, please try again",
+            error: error.message
         })
     }
+})
 
-    const hashedPassword = await bcrypt.hash(password, 5);
-    await User.create({
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        password: hashedPassword
-    })
+// signin endpoint - authenticate user
+userRouter.post("signin", async (req, res) => {
+    try {
+        const { email, password } = req.body;
 
-    res.status(201).json({
-        messaage: `${firstName}, you're succesfully signed up`
-    })
+        // find user
+        const user = await User.findOne({email: email});
+        if(!user){
+            return res.status(400).json({
+                message: "User not found"
+            })
+        }
+
+        // verify password
+        const truePassword = await bcrypt.compare(password, user.password);
+        if(!truePassword){
+            return res.status(400).json({
+                message: "Incorrect password"
+            })
+        }
+
+        // generate token
+        const payload = {
+            id: user._id,
+            exp: Date.now() + 1000 * 3600
+        }
+        const token = jwt.sign(payload, process.env.SECRET_KEY);
+
+        res.status(200).json({
+            message: `Welcome back ${user.firstName}`,
+            token: token,
+            user: {
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.email
+            }
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Login failed, please try again",
+            error: error.message
+        })
+    }
 })
