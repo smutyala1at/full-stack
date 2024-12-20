6b7f31591c5ddffa442794beefb77fcdd6d49186
Santosh Mutyala <santosh.mutyala@uni-oldenburg.de>
2024-12-20 09:33:10 +0100

    add user authentication to todos routes and implement get all todos endpoint

--- todos-project/backend/routes/todosRouter.js
+++ todos-project/backend/routes/todosRouter.js
@@ -1,10 +1,11 @@
 const { Todos } = require("../db/db");
 const { Router } = require("express");
+const { userAuthMiddleware } = require("../middlewares/userAuthMiddleware");
 
 const todosRouter = Router();
 
 // add todo endpoint
-todosRouter.post("/todo", async (req, res) => {
+todosRouter.post("/todo", userAuthMiddleware, async (req, res) => {
     try {
         const { title, description } = req.body;
         const userId = req.userId;
@@ -20,6 +21,32 @@ todosRouter.post("/todo", async (req, res) => {
             message: "Todo created successfully"
         })
 
+    } catch (error) {
+        res.status(500).json({
+            message: "Something went wrong, please try again",
+            error: error.message
+        })
+    }
+})
+
+// get all todos endpoint
+todosRouter.get("/todos", userAuthMiddleware, async (req, res) => {
+    try {
+        const result = [];
+
+        const todos = await Todos.findById({
+            userId: req.userId
+        })
+
+        todos.forEach(todo => result.push({
+            title: todo.title,
+            description: todo.description,
+            completed: todo.completed
+        }));
+
+        res.status(200).json({
+            todos: result
+        })
     } catch (error) {
         res.status(500).json({
             message: "Something went wrong, please try again",
