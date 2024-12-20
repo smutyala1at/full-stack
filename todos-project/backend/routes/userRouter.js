9161c3fc43a65afb43e639912055822a8683bad7
Santosh Mutyala <santosh.mutyala@uni-oldenburg.de>
2024-12-20 07:47:18 +0100

    add user signup functionality with password hashing

--- todos-project/backend/routes/userRouter.js
+++ todos-project/backend/routes/userRouter.js
@@ -1,17 +1,30 @@
 const { Router } = require("express");
+const bcrypt = require("bcrypt");
 const { User, Todos } = require("../db/db");
 
 
 const userRouter = Router();
 
-userRouter.post("/signup", (req, res) => {
+userRouter.post("/signup", async (req, res) => {
     const { firstName, lastName, email, password } = req.body;
 
     const user = User.findOne({ email: email });
 
     if (user) {
         return res.status(400).json({
-            message: "user already exists with this email";
+            message: `user with email ${email} already exists`
         })
     }
+
+    const hashedPassword = await bcrypt.hash(password, 5);
+    await User.create({
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: hashedPassword
+    })
+
+    res.status(201).json({
+        messaage: `${firstName}, you're succesfully signed up`
+    })
 })
