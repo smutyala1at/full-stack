4d6eb0c558a85521d2289d25f42627d706101629
Santosh Mutyala <santosh.mutyala@uni-oldenburg.de>
2025-01-08 12:48:33 +0100

    pickAPI: implement pick, partial and readonly utility types

---
+++ week-14/src/pickAPI.ts
@@ -0,0 +1,44 @@
+// Pick allows you to pick a subset of properties from an existing type/interface.
+
+interface User {
+    id: number;
+    name: string;
+    email: string;
+    password: string;
+}
+
+type UserProfile = Pick<User, "name" | "email">;
+
+const displayUserProfile = (user: User) : UserProfile => {
+    return {
+        name: user.name,
+        email: user.email
+    }
+}
+
+// optional properties - like zod.partial()
+// Partial allows you to make all properties of a type optional.
+type UpdateProps = Pick<User, "name" | "age" | "email">
+type UpdateUser = Partial<UpdateProps>;
+
+const updateUserProfile = (user: UpdateUser): void => {
+    // hit the db to update the user
+    console.log("User update successful")
+}
+
+
+// readonly properties 
+// when you have a configuration object that should not be altered after intialization, making it readonly ensuore its properties cannot be changed.
+
+interface Config {
+    endpoint: string;
+    apiKey: string;
+}
+
+const config: Readonly<Config> = {
+    endpoint: "https://api.example.com",
+    apiKey: "123456"
+}
+
+// config.apiKey = "1919191" 
+// error: Cannot assign to 'apiKey' because it is a read-only property.
