405dd6a02fde2464acdd6c745e65af7c76634fd7
Santosh Mutyala <santosh.mutyala.dev@gmail.com>
2025-03-20 12:31:06 +0100

    add try-catch block

--- week-17-postgres/dist/index.js
+++ week-17-postgres/dist/index.js
@@ -23,33 +23,38 @@ const pgClient = new pg_1.Client({
     database: process.env.DATABASE
 });
 const connectDB = () => __awaiter(void 0, void 0, void 0, function* () {
-    yield pgClient.connect();
-    console.log("db connected");
-    // create a table
-    yield pgClient.query(`
-        CREATE TABLE IF NOT EXISTS users(
-            Id SERIAL PRIMARY KEY,
-            name VARCHAR(50) NOT NULL,
-            email VARCHAR(255) NOT NULL UNIQUE,
-            password VARCHAR(255) NOT NULL
-        )
-    `);
-    console.log("users table created");
-    // add users into db table
-    /* await pgClient.query(`
-        INSERT INTO users(name, email, password)
-        VALUES ('santosh', 'santosh@gmail.com', 'santosh123'), ('saskia', 'saskia@gmail.com', 'saskia123')
-    `);
-    console.log("users added"); */
-    // get all users from db
-    const users = yield pgClient.query("SELECT * FROM users");
-    console.log(users.rows);
-    // update user details
-    const updateUser = yield pgClient.query(`
-        UPDATE users 
-        SET email='santoshm@gmail.com'
-        WHERE name='santosh'
-    `);
-    console.log("user updated", updateUser.rowCount);
+    try {
+        yield pgClient.connect();
+        console.log("db connected");
+        // create a table
+        yield pgClient.query(`
+            CREATE TABLE IF NOT EXISTS users(
+                Id SERIAL PRIMARY KEY,
+                name VARCHAR(50) NOT NULL,
+                email VARCHAR(255) NOT NULL UNIQUE,
+                password VARCHAR(255) NOT NULL
+            )
+        `);
+        console.log("users table created");
+        // add users into db table
+        yield pgClient.query(`
+            INSERT INTO users(name, email, password)
+            VALUES ('santosh', 'santosh@gmail.com', 'santosh123'), ('saskia', 'saskia@gmail.com', 'saskia123')
+        `);
+        console.log("users added");
+        // get all users from db
+        const users = yield pgClient.query("SELECT * FROM users");
+        console.log(users.rows);
+        // update user details
+        const updateUser = yield pgClient.query(`
+            UPDATE users 
+            SET email='santoshm@gmail.com'
+            WHERE name='santosh'
+        `);
+        console.log("user updated", updateUser.rowCount);
+    }
+    catch (error) {
+        console.log(error);
+    }
 });
 connectDB();
