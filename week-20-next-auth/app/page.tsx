ce929683bb2e53c24d5baa64a427b0742a2b32b7
Santosh Mutyala <santosh.mutyala.dev@gmail.com>
2025-05-01 21:08:20 +0200

    useSession hook to get auth status

--- week-20-next-auth/app/page.tsx
+++ week-20-next-auth/app/page.tsx
@@ -1,9 +1,21 @@
-import Image from "next/image";
+"use client"
+import { SessionProvider, signIn, signOut, useSession } from "next-auth/react"
 
 export default function Home() {
   return (
-    <div>
-      Main Page
-    </div>
+    <SessionProvider>
+      <AuthStatus />
+    </SessionProvider>
   );
+}
+
+function AuthStatus(){
+  const { status } = useSession();
+
+  return (
+    <div>
+      { status == "authenticated" && <button onClick={() => signOut()}>Logout</button> }
+      { status == "unauthenticated" && <button onClick={() => signIn()}>Logout</button> }
+    </div>
+  )
 }
