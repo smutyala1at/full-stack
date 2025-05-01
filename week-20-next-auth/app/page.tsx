f05ba3f5d7b09da3798512b58e3477a3d7974d58
Santosh Mutyala <santosh.mutyala.dev@gmail.com>
2025-05-01 21:27:02 +0200

    server side render auth status using getServerSession

--- week-20-next-auth/app/page.tsx
+++ week-20-next-auth/app/page.tsx
@@ -1,7 +1,8 @@
-"use client"
-import { SessionProvider, signIn, signOut, useSession } from "next-auth/react"
+// "use client"
+// import { SessionProvider, signIn, signOut, useSession } from "next-auth/react"
+import { getServerSession } from "next-auth/next"
 
-export default function Home() {
+/* export default function Home() {
   return (
     <SessionProvider>
       <AuthStatus />
@@ -16,6 +17,15 @@ function AuthStatus(){
     <div>
       { status == "authenticated" && <button onClick={() => signOut()}>Logout</button> }
       { status == "unauthenticated" && <button onClick={() => signIn()}>Logout</button> }
+    </div>
+  )
+} */
+
+export default async function App() {
+  const session = await getServerSession();
+  return (
+    <div>
+      {JSON.stringify(session?.user?.email)}
     </div>
   )
 }
