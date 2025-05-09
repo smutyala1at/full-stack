52453cca386b4ddf8c06230e330eb9b647494d13
smutyala1at <santosh.mutyala.dev@gmail.com>
2025-05-09 21:10:01 +0200

    init frontend with tailwind

---
+++ paytm/frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="bg-amber-600">Hello world</div>
+  )
+}
+
+export default App
